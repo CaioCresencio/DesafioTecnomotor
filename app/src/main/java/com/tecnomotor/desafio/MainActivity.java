f6e3bc23f257de2d98b1fd8622ac3131921b8182
CaioCresencio <caiocresencio98@gmail.com>
2020-03-16 20:58:31 -0300

    Criação do Service que busca os tipos

--- app/src/main/java/com/tecnomotor/desafio/MainActivity.java
+++ app/src/main/java/com/tecnomotor/desafio/MainActivity.java
@@ -3,12 +3,30 @@ package com.tecnomotor.desafio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.tecnomotor.desafio.service.HTTPServiceTipo;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        HTTPServiceTipo serviceTipo = new HTTPServiceTipo();
+        try {
+            List<String> lista = serviceTipo.execute().get();
+            textView.setText(lista.get(0));
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 }
