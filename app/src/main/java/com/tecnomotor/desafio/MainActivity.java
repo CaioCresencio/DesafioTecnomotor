896a96a6e567f486e5a54f3721ff34b5439cbd34
CaioCresencio <caiocresencio98@gmail.com>
2020-03-16 21:16:04 -0300

    Criação do service e do model das montadoras

--- app/src/main/java/com/tecnomotor/desafio/MainActivity.java
+++ app/src/main/java/com/tecnomotor/desafio/MainActivity.java
@@ -3,8 +3,11 @@ package com.tecnomotor.desafio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.tecnomotor.desafio.model.Montadora;
+import com.tecnomotor.desafio.service.HTTPServiceMontadora;
 import com.tecnomotor.desafio.service.HTTPServiceTipo;
 
 import java.util.List;
@@ -12,6 +15,9 @@ import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
+    private List<Montadora> mmontadoras;
+    private Spinner spinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,9 +25,13 @@ public class MainActivity extends AppCompatActivity {
 
         textView = findViewById(R.id.textView);
         HTTPServiceTipo serviceTipo = new HTTPServiceTipo();
+        HTTPServiceMontadora serviceMontadora;
         try {
             List<String> lista = serviceTipo.execute().get();
-            textView.setText(lista.get(0));
+            serviceMontadora = new HTTPServiceMontadora(lista.get(0));
+            mmontadoras = serviceMontadora.execute().get();
+            textView.setText(mmontadoras.get(0).getNome());
+
         } catch (ExecutionException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
---
+++ app/src/main/java/com/tecnomotor/desafio/model/Montadora.java
@@ -0,0 +1,38 @@
+package com.tecnomotor.desafio.model;
+
+public class Montadora {
+
+    private int id;
+    private String nome;
+    private String tipo;
+
+    public Montadora(int id, String nome, String tipo){
+        this.setId(id);
+        this.setNome(nome);
+        this.setTipo(tipo);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
